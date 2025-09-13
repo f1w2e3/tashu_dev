c7784533fcdf86865400f0c2f35ed74db37b54cc
aeu01 <ringcpython@gmail.com>
2025-09-13 11:58:35 +0900

    테스트 코드 작성

--- predictions/tests.py
+++ predictions/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+# Create your tests here
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from .models import Prediction
+
+class PredictionModelTest(TestCase):
+    def test_prediction_creation(self):
+        """Prediction 객체가 정상적으로 생성되는지 확인"""
+        prediction = Prediction.objects.create(
+            station_id=1,
+            predicted_bikes=10,
+            predicted_time=timezone.now()
+        )
+        self.assertEqual(prediction.station_id, 1)
+        self.assertEqual(prediction.predicted_bikes, 10)
+        self.assertIsNotNone(prediction.predicted_time)
